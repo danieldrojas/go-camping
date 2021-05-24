896bc2c25cb4c2b98a92072c2713b3f015f21b28
Daniel Rojas <daniel.drojas@outlook.com>
2021-05-24 13:37:04 -0500

    add stop coors to trip obj

--- index.js
+++ index.js
@@ -1,52 +1,66 @@
+//set the init function available to the window.
 window.initMap = initMap
 
+// all calculations are done using meters
+
+//variables and constants
 let map;
+let trip = {};
+const originField = document.getElementById("origin");
+const destinationField = document.getElementById("destination");
+const submitButton = document.getElementById("searchBtn");
+const milesField = document.getElementById("miles-day")
 
 //map instance 
 function initMap() {
-    const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer();
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 35.2784, lng: -93.1338 },
         zoom: 8,
     });
-    directionsRenderer.setMap(map);
-
-
-    submitButton.addEventListener("click", function (e) {
-        directions(e, directionsRenderer, directionsService)
+    submitButton.addEventListener("click", e => {
+        directions(e)
     })
-
-
 }
 
-//input UI
-const originField = document.getElementById("origin");
-const destinationField = document.getElementById("destination");
-const submitButton = document.getElementById("searchBtn")
+
+//TODO: delete, these are for development 
+let origin = "133+richland+circle+Russellville+AR";
+let destination = "2370+main+street+northwest+duluth+ga";
+let miles = 495631
+trip.origin = origin
+trip.destination = destination
+trip.traveledTarget = miles;
 
 
 
-
-function directions(e, directionsRenderer, directionsService) {
+function directions(e) {
     e.preventDefault()
-    let origin = originField.value;
-    let destination = destinationField.value;
 
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+
+    // let origin = originField.value;
+    // let destination = destinationField.value;
+
+    //pass miles to meters
+    // let miles = milesField.value*1609.34;
 
     directionsService.route(
         {
-            origin: {
-                query: origin
-            },
-            destination: {
-                query: destination
-            },
+            origin: origin,
+            destination: destination,
             travelMode: google.maps.TravelMode.DRIVING,
         },
         (response, status) => {
             if (status === "OK") {
+                trip = response.routes[0].legs[0];
+
                 directionsRenderer.setDirections(response);
+                directionsRenderer.setMap(map);
+
+                if (miles) {
+                    getPointAtMiles()
+                }
             } else {
                 window.alert("Directions request failed due to " + status);
             }
@@ -54,12 +68,80 @@ function directions(e, directionsRenderer, directionsService) {
     );
 }
 
+//get a point on the route at miles
+function getPointAtMiles() {
+    // getRouteStep
+    let total = 0;
+    for (let i = 0; i < trip.steps.length; i++) {
+        total += trip.steps[i].distance.value;
+        if (total >= miles) {
+            trip.steps[i].distance.distance_from_origin = total
+            trip.stops = [trip.steps[i]];
+            break;
+        }
+    }
+    let stop = trip.stops[0];
+    let distanceTrack = stop.distance.value;
+    let distance_from_origin = stop.distance.distance_from_origin;
+    let ratio = (miles - (distance_from_origin - distanceTrack)) / distanceTrack;
+    //Aprox LatLng at stop 
+    let pathIndex = Math.ceil(ratio * stop.path.length);
+    let LatLngAtStop = stop.path[pathIndex]
+
+    stop.stop_point_coors = LatLngAtStop
+    console.log(trip)
+
+    //places
+    // console.log("map: ", map)
+
+    // const placesService = new google.maps.places.PlacesService(map);
+    // placesService.findPlaceFromQuery(request, callback)
+    // console.log(placesService)
+
+    // let placePhe = placesService.findPlaceFromNumber(request)
+
+    // console.log(placePhe)
+
+
+
+    // let ratio = trip.distance. / trip.distance.value;
+    // console.log(ratio) //debugged ok
+    //find an initial point to start iteration at array[i]
+    // let index = Math.ceil(ratio * trip.steps.length);
+    // console.log("index: ", index);
+    // let step = trip.steps[index];
+    // console.log(step)
 
 
 
 
 
 
+    // console.log("trip with index: ", index, steps[index])
+    // let { end_location: end_location,
+    //     start_location: start_location } = steps[index];
+    // console.log("end_location: ", end_location, "start_location: ", start_location)
 
 
+    // if (start_location > miles > end_location) {
+    //     return
+    // }
+
+
+    // iteration(index - 1);
+
+
+
+
+}
+
+
+function getDistanceBetweenPoints(startLatLng, endLatLng) {
+    return google.maps.geometry.spherical.computeDistanceBetween(startLatLng, endLatLng)
+
+}
+
+function getDistancePath(path) {
+    return google.maps.geometry.spherical.computeLength(path)
+}
 
