c074594f18e4f930b62b0d5d26b4ba5d8bf44fb8
Daniel Rojas <daniel.drojas@outlook.com>
2021-05-28 19:24:32 -0500

    change type of add to route input to checkbox

--- assets/template/infoHtml.js
+++ assets/template/infoHtml.js
@@ -15,7 +15,7 @@ export default function createInfoHTML({ name, rating, adr_address, formatted_ph
         ${adr_address}<br>
   <span>${formatted_phone_number}</span>
         <span>${Math.round(distanceToCampground / 1609)} miles </span><br>
-        <label for="addBtn" name="campgroundOptions"> <input id="addBtn" name="campgroundOptions" type="radio" value=${place_id}>Add to route</label>
+        <label for="addBtn" name="campgroundOptions"> <input id="addBtn" name="campgroundOptions" type="checkbox" value=${place_id}>Add to route</label>
           <a target="_blank" href="${url}"><button>More</button></a>
       `;
 };
